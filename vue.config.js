5a5a8b64905c1a376b62c0fe52fcdd5aa629c7de
bill85831 <bill85831@gmail.com>
2021-10-27 16:21:20 +0800

    change publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   lintOnSave: false,
-  publicPath: "./",
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/shopping/'
+    : './',
   configureWebpack: {
     externals: { jquery: "$" },
   },
